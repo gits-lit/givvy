c181825144d21c76db13d548b980d9368c0af86d
leestanley <stanleylee91776@gmail.com>
2021-01-30 23:44:46 -0800

    homepage done

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import './style.scss';
-import bg from '../../assets/hbg.png';
+import { Button } from 'antd';
+import iphone from '../../assets/iphone.png';
 
-const HomePage= () => {
+import './style.scss';
+
+const HomePage = () => {
   return (
     <div className="home-page">
-      <NavLink to="/donate">
-        <button>Start now</button>
-      </NavLink>
-      <NavLink to="/donate">
-        <button>Demo now</button>
-      </NavLink>
+      <img src={iphone} alt="iphone" className="iphone" />
+      <div className="homenav">
+        <h2>givvy</h2>
+        <div className="links">
+          <h4>Locations</h4>
+          <h4>Analytics</h4>
+          <h4>Pricing</h4>
+        </div>
+        <Button className="signin" size="large">
+          Sign in >
+        </Button>
+      </div>
+      <div className="content">
+        <h1>Community</h1>
+        <h1>Through Donation</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. 
+        </p>
+        <div className="buttons">
+          <NavLink to="/donate">
+            <Button className="play1" size="big">Start now ></Button>
+          </NavLink>
+          <NavLink to="/donate">
+            <Button  className="play" size="big">Demo now ></Button>
+          </NavLink>
+        </div>
+      </div>
+      <p className="credits">Made at HackUCI w/🍵</p>
     </div>
   );
 };
